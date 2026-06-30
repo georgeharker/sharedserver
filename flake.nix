8d8de646fa793dc3937b15d99465dd4158b3ad6c
George Harker <george@george-graphics.co.uk>
2026-06-30 15:13:33 -0700

    build(nix): add treefmt (stylua/nixfmt/statix) to flake; add .stylua.toml

--- flake.nix
+++ flake.nix
@@ -8,65 +8,100 @@
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs:
-    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
-      imports = [inputs.flake-parts.flakeModules.easyOverlay];
-      perSystem = {
-        lib,
-        config,
-        pkgs,
-        ...
-      }: let
-        cargoToml = builtins.fromTOML (builtins.readFile ./rust/Cargo.toml);
-        inherit (cargoToml.package) name description repository;
-        meta = {
-          inherit description;
-          mainProgram = name;
-          homepage = repository;
-          license = lib.licenses.mit;
-        };
-        craneLib = inputs.crane.mkLib pkgs;
-        sourceFilter = path: type: (builtins.match ".*/test_helpers/.*\\.sh$" path != null) || (craneLib.filterCargoSources path type);
-        cargoArtifacts = craneLib.buildDepsOnly {
-          src = ./rust;
-          strictDeps = true;
-        };
-        sharedserver = craneLib.buildPackage {
-          inherit cargoArtifacts meta;
-          src = lib.cleanSourceWith {
-            src = ./.;
-            filter = sourceFilter;
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      imports = [
+        inputs.flake-parts.flakeModules.easyOverlay
+        inputs.treefmt-nix.flakeModule
+      ];
+      perSystem =
+        {
+          lib,
+          config,
+          pkgs,
+          ...
+        }:
+        let
+          cargoToml = builtins.fromTOML (builtins.readFile ./rust/Cargo.toml);
+          inherit (cargoToml.package) name description repository;
+          meta = {
+            inherit description;
+            mainProgram = name;
+            homepage = repository;
+            license = lib.licenses.mit;
           };
-          cargoToml = rust/Cargo.toml;
-          cargoLock = rust/Cargo.lock;
-          postUnpack = ''
-            cd $sourceRoot/rust
-            sourceRoot="."
-          '';
-          # Isolate tests from the host's live lockdir (the Nix sandbox is off
-          # by default on Darwin, so tests would otherwise hit /tmp/sharedserver)
-          preCheck = ''
-            export SHAREDSERVER_LOCKDIR="$TMPDIR/sharedserver-tests"
-          '';
-        };
-        sharedserver-nvim = pkgs.vimUtils.buildVimPlugin {
-          pname = "sharedserver-nvim";
-          version = toString (inputs.self.shortRev or inputs.self.dirtyShortRev or inputs.self.lastModified or "git");
-          src = with lib.fileset;
-            toSource {
-              root = ./.;
-              fileset = unions (map maybeMissing [./lua ./plugin ./ftdetect ./doc]);
+          craneLib = inputs.crane.mkLib pkgs;
+          sourceFilter =
+            path: type:
+            (builtins.match ".*/test_helpers/.*\\.sh$" path != null) || (craneLib.filterCargoSources path type);
+          cargoArtifacts = craneLib.buildDepsOnly {
+            src = ./rust;
+            strictDeps = true;
+          };
+          sharedserver = craneLib.buildPackage {
+            inherit cargoArtifacts meta;
+            src = lib.cleanSourceWith {
+              src = ./.;
+              filter = sourceFilter;
             };
+            cargoToml = rust/Cargo.toml;
+            cargoLock = rust/Cargo.lock;
+            postUnpack = ''
+              cd $sourceRoot/rust
+              sourceRoot="."
+            '';
+            # Isolate tests from the host's live lockdir (the Nix sandbox is off
+            # by default on Darwin, so tests would otherwise hit /tmp/sharedserver)
+            preCheck = ''
+              export SHAREDSERVER_LOCKDIR="$TMPDIR/sharedserver-tests"
+            '';
+          };
+          sharedserver-nvim = pkgs.vimUtils.buildVimPlugin {
+            pname = "sharedserver-nvim";
+            version = toString (
+              inputs.self.shortRev or inputs.self.dirtyShortRev or inputs.self.lastModified or "git"
+            );
+            src =
+              with lib.fileset;
+              toSource {
+                root = ./.;
+                fileset = unions (
+                  map maybeMissing [
+                    ./lua
+                    ./plugin
+                    ./ftdetect
+                    ./doc
+                  ]
+                );
+              };
+          };
+        in
+        {
+          packages = {
+            inherit sharedserver sharedserver-nvim;
+            default = sharedserver;
+          };
+          overlayAttrs = config.packages;
+          treefmt = {
+            programs = {
+              nixfmt.enable = true;
+              statix.enable = true;
+              stylua.enable = true;
+            };
+          };
         };
-      in {
-        packages = {
-          inherit sharedserver sharedserver-nvim;
-          default = sharedserver;
-        };
-        overlayAttrs = config.packages;
-      };
     };
 }
